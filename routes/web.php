47f890886848771de997fd9870aa9b621e34dae7
adriano ansaloni <adrianoansaloni@gmail.com>
2023-08-17 22:11:16 -0300

    Create routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PessoaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PessoaController::class, 'index']);
+Route::get('/create', [PessoaController::class, 'create']);
+Route::post('/', [PessoaController::class, 'store']);
+Route::get('/{id}/edit', [PessoaController::class, 'edit']);
+Route::put('/{id}', [PessoaController::class, 'update']);
+Route::delete('/{id}', [PessoaController::class, 'destroy']);
