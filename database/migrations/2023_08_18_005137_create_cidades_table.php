96134745d120763e8bef150779e59bf63c8a328f
adriano ansaloni <adrianoansaloni@gmail.com>
2023-08-17 22:00:33 -0300

    Edit cidades migration

--- database/migrations/2023_08_18_005137_create_cidades_table.php
+++ database/migrations/2023_08_18_005137_create_cidades_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -13,8 +14,15 @@ return new class extends Migration
     {
         Schema::create('cidades', function (Blueprint $table) {
             $table->id();
+            $table->string('nome', 60);
             $table->timestamps();
         });
+
+        DB::table('cidades')->insert([
+            ['nome' => 'Belo Horizonte'],
+            ['nome' => 'Betim'],
+            ['nome' => 'Contagem'],
+        ]);
     }
 
     /**
