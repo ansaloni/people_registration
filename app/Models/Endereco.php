f5a52ae6a9468bd2698a919a7cb3509bd0a8f938
adriano ansaloni <adrianoansaloni@gmail.com>
2023-08-17 22:10:03 -0300

    Edit Endereco model

--- app/Models/Endereco.php
+++ app/Models/Endereco.php
@@ -7,5 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Endereco extends Model
 {
-    use HasFactory;
+    protected $fillable = ['tipo_logradouro_id', 'cidade_id', 'pessoa_id', 'logradouro', 'numero', 'cep', 'bairro'];
+
+    public function tipoLogradouro()
+    {
+        return $this->belongsTo(TipoLogradouro::class, 'tipo_logradouro_id');
+    }
+
+    public function cidade()
+    {
+        return $this->belongsTo(Cidade::class, 'cidade_id');
+    }
+
+    public function pessoa()
+    {
+        return $this->belongsTo(Pessoa::class, 'pessoa_id');
+    }
 }
